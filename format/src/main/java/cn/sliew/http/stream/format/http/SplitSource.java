ce106568c832d246779119a7b7de7ef1f4433e64
wangqi <wangqi@xinc818.group>
2022-04-14 17:57:49 +0800

    source, sink and transform plugin.

--- format/src/main/java/cn/sliew/http/stream/format/http/SplitSource.java
+++ format/src/main/java/cn/sliew/http/stream/format/http/SplitSource.java
@@ -3,12 +3,9 @@ package cn.sliew.http.stream.format.http;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public interface MapReduceSource<Root, Sub, T> {
+public interface SplitSource<Root, Sub, T> {
 
     List<Sub> map(Root rootTask);
 
     CompletableFuture<T> process(Sub subTask);
-
-    ProcessResult reduce(ProcessResult result);
-
 }
