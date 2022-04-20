f826a211a8932ebd29d277b0cb823b0a64640495
wangqi <1942460489@qq.com>
2022-04-20 21:29:30 +0800

    github connector.

--- flink/src/main/java/cn/sliew/http/stream/flink/github/commit/CommitHeaders.java
+++ flink/src/main/java/cn/sliew/http/stream/flink/github/commit/CommitHeaders.java
@@ -5,6 +5,9 @@ import org.apache.flink.runtime.rest.messages.EmptyRequestBody;
 import org.apache.flink.runtime.rest.messages.MessageHeaders;
 import org.apache.flink.shaded.netty4.io.netty.handler.codec.http.HttpResponseStatus;
 
+/**
+ * fixme 这玩意在发送的时候会自动添加版本号 v1 到 url 路径中
+ */
 public class CommitHeaders
         implements MessageHeaders<EmptyRequestBody, CommitResponseBody, CommitMessageParameters> {
 
--- web/src/main/java/cn/sliew/http/stream/web/controller/remote/GithubController.java
+++ web/src/main/java/cn/sliew/http/stream/web/controller/remote/GithubController.java
@@ -1,19 +1,10 @@
 package cn.sliew.http.stream.web.controller.remote;
 
-import cn.sliew.http.stream.akka.util.JacksonUtil;
-import cn.sliew.http.stream.flink.github.commit.CommitHeaders;
-import cn.sliew.http.stream.flink.github.commit.CommitMessageParameters;
-import cn.sliew.http.stream.flink.github.commit.CommitResponseBody;
 import cn.sliew.http.stream.remote.github.GithubRemoteService;
 import cn.sliew.http.stream.remote.github.client.commit.CommitListDO;
 import cn.sliew.http.stream.remote.github.client.commit.CommitListQuery;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.runtime.rest.RestClient;
-import org.apache.flink.runtime.rest.RestClientConfiguration;
-import org.apache.flink.runtime.rest.messages.EmptyRequestBody;
-import org.apache.flink.runtime.util.ExecutorThreadFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,16 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/remote/github")
 @Tag(name = "/remote/github", description = "github 接口")
 public class GithubController {
-
-    private final ExecutorService executorService = Executors.newFixedThreadPool(4, new ExecutorThreadFactory("Flink-RestClusterClient-IO"));
 
     @Autowired
     private GithubRemoteService githubRemoteService;
@@ -39,16 +25,7 @@ public class GithubController {
     @ApiOperation("查询仓库提交记录")
     public List<CommitListDO> listCommits(@PathVariable("owner") String owner,
                                           @PathVariable("repo") String repo,
-                                          CommitListQuery query) throws Exception {
-
-        RestClientConfiguration configuration = RestClientConfiguration.fromConfiguration(new Configuration());
-        RestClient client = new RestClient(configuration, executorService);
-        CommitMessageParameters parameters = new CommitMessageParameters();
-        parameters.owner.resolveFromString(owner);
-        parameters.repo.resolveFromString(repo);
-        CompletableFuture<CommitResponseBody> future = client.sendRequest("api.github.com", 80, CommitHeaders.getInstance(), parameters, EmptyRequestBody.getInstance());
-        CommitResponseBody commitResponseBody = future.get();
-        System.out.println(JacksonUtil.toJsonString(commitResponseBody));
+                                          CommitListQuery query) {
         return githubRemoteService.listCommits(owner, repo, query);
     }
 }
