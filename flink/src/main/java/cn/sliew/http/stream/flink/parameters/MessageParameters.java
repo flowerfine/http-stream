839ed9273dfd4b8fa633536c02c43e881632aead
wangqi <1942460489@qq.com>
2022-04-21 00:13:26 +0800

    http connector

---
+++ flink/src/main/java/cn/sliew/http/stream/flink/parameters/MessageParameters.java
@@ -0,0 +1,16 @@
+package cn.sliew.http.stream.flink.parameters;
+
+import org.apache.http.NameValuePair;
+
+public class MessageParameters {
+
+    private NameValuePair[] headers;
+    private NameValuePair[] paths;
+    private NameValuePair[] querys;
+    private NameValuePair[] bodys;
+
+    public String getUrl() {
+        return null;
+    }
+
+}
