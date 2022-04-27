2ca67548c7609b2d70371b4cab8ad96dc48559ed
wangqi <1942460489@qq.com>
2022-04-27 08:59:27 +0800

    http connector

--- arrow-json/src/main/java/cn/sliew/http/stream/arrow/json/Main.java
+++ arrow-json/src/main/java/cn/sliew/http/stream/arrow/json/Main.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.ipc.ArrowStreamWriter;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -19,8 +21,15 @@ public class Main {
         final JsonToArrowConfig jsonToArrowConfig = new JsonToArrowConfigBuilder(new RootAllocator()).build();
         final JsonToArrowVectorIterator2 iterator = new JsonToArrowVectorIterator2(parser, jsonToArrowConfig);
 
+
         while (iterator.hasNext()) {
             VectorSchemaRoot vectorSchemaRoot = iterator.next();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            final ArrowStreamWriter arrowStreamWriter = new ArrowStreamWriter(vectorSchemaRoot, jsonToArrowConfig.getProvider(), outputStream);
+            arrowStreamWriter.start();
+            arrowStreamWriter.writeBatch();
+            arrowStreamWriter.close();
+            System.out.println(outputStream);
             System.out.println("输出转化结果! 数量: " + vectorSchemaRoot.getRowCount());
         }
 
