1a90fb66ae74484b2c317d3b55e7ae51fa866eae
wangqi <1942460489@qq.com>
2022-05-12 00:54:43 +0800

    temp commit.

--- flink/src/main/java/cn/sliew/http/stream/flink/util/HttpRequestMethod.java
+++ flink/src/main/java/cn/sliew/http/stream/flink/util/HttpRequestMethod.java
@@ -4,103 +4,58 @@
 
 package cn.sliew.http.stream.flink.util;
 
+import cn.sliew.milky.common.parse.placeholder.PropertyPlaceholder;
 import cn.sliew.milky.common.util.JacksonUtil;
+import cn.sliew.milky.log.Logger;
+import cn.sliew.milky.log.LoggerFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import okhttp3.HttpUrl;
+import okhttp3.MediaType;
 import okhttp3.Request;
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.core.util.JsonUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import okhttp3.RequestBody;
+import okhttp3.internal.Util;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
 
 
 /**
  * utility to facilitate different params: headers、paths、querys and body param.
  */
 public enum HttpRequestMethod {
+
     GET("GET") {
         @Override
-        protected Request getHttpRequestContentJson(String uriTemplate,
-                                                    JsonObject parameters, JsonObject payloads)
-                throws UnsupportedEncodingException {
-            Pair<String, JsonObject> replaced = replaceVariables(uriTemplate, parameters);
-            //ignore payloads
-            return new HttpGet(appendQueryParams(replaced.getKey(), replaced.getValue()));
-        }
-
-        @Override
-        protected HttpUriRequest getHttpRequestContentUrlEncoded(String uriTemplate, JsonObject parameters)
-                throws UnsupportedEncodingException {
-            return getHttpRequestContentJson(uriTemplate, parameters, new JsonObject());
+        protected Request.Builder getRequest(RequestBody body) {
+            return new Request.Builder().get();
         }
     },
 
     POST("POST") {
         @Override
-        protected HttpUriRequest getHttpRequestContentJson(String uriTemplate,
-                                                           JsonObject parameters, JsonObject payloads)
-                throws UnsupportedEncodingException {
-            Pair<String, JsonObject> replaced = replaceVariables(uriTemplate, parameters);
-            for (Map.Entry<String, JsonElement> entry : payloads.entrySet()) {
-                replaced.getValue().add(entry.getKey(), entry.getValue());
-            }
-            return setEntity(new HttpPost(replaced.getKey()), replaced.getValue().toString());
-        }
-
-        @Override
-        protected HttpUriRequest getHttpRequestContentUrlEncoded(String uriTemplate, JsonObject parameters)
-                throws UnsupportedEncodingException {
-            Pair<String, JsonObject> replaced = replaceVariables(uriTemplate, parameters);
-            return setEntity(new HttpPost(replaced.getKey()), jsonToUrlEncodedEntity(replaced.getValue()));
+        protected Request.Builder getRequest(RequestBody body) {
+            return new Request.Builder().post(body);
         }
     },
 
     PUT("PUT") {
         @Override
-        protected HttpUriRequest getHttpRequestContentJson(String uriTemplate,
-                                                           JsonObject parameters, JsonObject payloads)
-                throws UnsupportedEncodingException {
-            Pair<String, JsonObject> replaced = replaceVariables(uriTemplate, parameters);
-            for (Map.Entry<String, JsonElement> entry : payloads.entrySet()) {
-                replaced.getValue().add(entry.getKey(), entry.getValue());
-            }
-            return setEntity(new HttpPut(replaced.getKey()), replaced.getValue().toString());
-        }
-
-        @Override
-        protected HttpUriRequest getHttpRequestContentUrlEncoded(String uriTemplate, JsonObject parameters)
-                throws UnsupportedEncodingException {
-            Pair<String, JsonObject> replaced = replaceVariables(uriTemplate, parameters);
-            return setEntity(new HttpPut(replaced.getKey()), jsonToUrlEncodedEntity(replaced.getValue()));
+        protected Request.Builder getRequest(RequestBody body) {
+            return new Request.Builder().put(body);
         }
     },
 
     DELETE("DELETE") {
         @Override
-        protected HttpUriRequest getHttpRequestContentJson(String uriTemplate,
-                                                           JsonObject parameters, JsonObject payloads)
-                throws UnsupportedEncodingException {
-            Pair<String, JsonObject> replaced = replaceVariables(uriTemplate, parameters);
-            for (Map.Entry<String, JsonElement> entry : payloads.entrySet()) {
-                replaced.getValue().add(entry.getKey(), entry.getValue());
-            }
-            return new HttpDelete(replaced.getKey());
-        }
-
-        @Override
-        protected HttpUriRequest getHttpRequestContentUrlEncoded(String uriTemplate, JsonObject parameters)
-                throws UnsupportedEncodingException {
-            Pair<String, JsonObject> replaced = replaceVariables(uriTemplate, parameters);
-            return new HttpDelete(replaced.getKey());
+        protected Request.Builder getRequest(RequestBody body) {
+            return new Request.Builder().delete(body);
         }
     };
 
-    private static final Logger LOG = LoggerFactory.getLogger(HttpRequestMethod.class);
+    private static final Logger log = LoggerFactory.getLogger(HttpRequestMethod.class);
     private final String name;
 
     HttpRequestMethod(String name) {
@@ -112,119 +67,69 @@ public enum HttpRequestMethod {
         return name;
     }
 
-    /**
-     * This is the public method to generate HttpUriRequest for each type of Http Method
-     *
-     * @param uriTemplate input URI, which might contain place holders
-     * @param parameters  parameters to be add to URI or to request Entity
-     * @param headers     Http header tags
-     * @return HttpUriRequest ready for connection
-     */
-    public HttpUriRequest getHttpRequest(final String uriTemplate, final JsonObject parameters, final Map<String, String> headers)
-            throws UnsupportedEncodingException {
-        return getHttpRequest(uriTemplate, parameters, headers, new JsonObject());
-    }
-
-    /**
-     * This is the public method to generate HttpUriRequest for each type of Http Method
-     *
-     * @param uriTemplate input URI, which might contain place holders
-     * @param headers     Http header tags
-     * @param parameters  parameters to be add to URI or to request Entity
-     * @param payloads    additional payloads to be included in the body of the Http request
-     * @return HttpUriRequest ready for connection
-     */
     public Request getRequest(final String uriTemplate,
-                              final Map<String, String> headers,
                               final ObjectNode parameters,
-                              final ObjectNode payloads)
-            throws UnsupportedEncodingException {
-        Request request;
-
-        // substitute variables in headers
-        Map<String, String> headersCopy = new HashMap<>();
-        ObjectNode parametersCopy = JacksonUtil.deepCopy(parameters).getAsJsonObject();
-        for (Map.Entry<String, String> entry : headers.entrySet()) {
-            Pair<String, ObjectNode> replaced = VariableUtils.replaceWithTracking(entry.getValue(), parameters);
-            if (!replaced.getLeft().equals(entry.getValue())) {
-                parametersCopy = JsonUtils.deepCopy(replaced.getRight()).getAsJsonObject();
-                headersCopy.put(entry.getKey(), replaced.getLeft());
-                LOG.info("Substituted header string: {} = {}", entry.getKey(), replaced.getLeft());
-            } else {
-                headersCopy.put(entry.getKey(), entry.getValue());
-            }
-        }
-
-        LOG.info("Final parameters for HttpRequest: {}", parametersCopy.toString());
-        if (headersCopy.containsKey("Content-Type")
-                && headersCopy.get("Content-Type").equals("application/x-www-form-urlencoded")) {
-            request = getRequest(uriTemplate, parametersCopy);
-        } else {
-            request = getRequest(uriTemplate, parametersCopy, payloads);
-        }
-
-        for (Map.Entry<String, String> entry : headersCopy.entrySet()) {
-            request.addHeader(entry.getKey(), entry.getValue());
-        }
-        return request;
+                              final Map<String, String> headers,
+                              final ObjectNode querys,
+                              final ObjectNode payloads) {
+        Request.Builder requestBuilder = getRequest(getRequestBody(parameters, payloads));
+        appendQuerys(requestBuilder, uriTemplate, parameters, querys);
+        appendHeaders(requestBuilder, parameters, headers);
+        return requestBuilder.build();
     }
 
-    protected abstract Request getRequest(String uriTemplate, ObjectNode parameters, ObjectNode payloads)
-            throws UnsupportedEncodingException;
 
-    protected abstract Request getRequest(String uriTemplate, ObjectNode parameters)
-            throws UnsupportedEncodingException;
-
-    protected Pair<String, ObjectNode> replaceVariables(String uriTemplate, ObjectNode parameters)
-            throws UnsupportedEncodingException {
-        return null;
-    }
-
-    protected String appendQueryParams(String url, ObjectNode params) {
+    protected void appendQuerys(Request.Builder requestBuilder, String urlTemplate, ObjectNode parameters, ObjectNode querys) {
         try {
+            String url = replace(urlTemplate, parameters);
             HttpUrl.Builder builder = HttpUrl.get(url).newBuilder();
-            Iterator<Map.Entry<String, JsonNode>> iterator = params.fields();
+            Iterator<Map.Entry<String, JsonNode>> iterator = querys.fields();
             while (iterator.hasNext()) {
                 Map.Entry<String, JsonNode> entry = iterator.next();
-                builder.addQueryParameter(entry.getKey(), entry.getValue().toString());
+                String replaced = replace(entry.getValue().toString(), parameters);
+                builder.addQueryParameter(entry.getKey(), replaced);
             }
-            return builder.build().toString();
+            requestBuilder.url(builder.build());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    /**
-     * Convert Json formatted parameter set to Url Encoded Entity as requested by
-     * Content-Type: application/x-www-form-urlencoded
-     * Json Example:
-     * {"param1": "value1", "param2": "value2"}
-     * <p>
-     * URL Encoded Example:
-     * param1=value1&param2=value2
-     *
-     * @param parameters Json structured parameters
-     * @return URL encoded entity
-     */
-    protected UrlEncodedFormEntity jsonToUrlEncodedEntity(JsonObject parameters) {
-        try {
-            List<NameValuePair> nameValuePairs = new ArrayList<>();
-            for (Map.Entry<String, JsonElement> entry : parameters.entrySet()) {
-                nameValuePairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue().getAsString()));
-            }
-            return new UrlEncodedFormEntity(nameValuePairs, StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    protected void appendHeaders(Request.Builder requestBuilder, ObjectNode parameters, Map<String, String> headers) {
+        Map<String, String> headersCopy = new HashMap<>();
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            String replaced = replace(entry.getValue(), parameters);
+            headersCopy.put(entry.getKey(), replaced);
+        }
+        for (Map.Entry<String, String> entry : headersCopy.entrySet()) {
+            requestBuilder.header(entry.getKey(), entry.getValue());
         }
     }
 
-    protected Request setEntity(HttpEntityEnclosingRequestBase requestBase, String stringEntity)
-            throws UnsupportedEncodingException {
-        return setEntity(requestBase, new StringEntity(stringEntity));
+    private RequestBody getRequestBody(ObjectNode parameters, ObjectNode body) {
+        if (body == null || body.isEmpty()) {
+            return Util.EMPTY_REQUEST;
+        }
+        ObjectNode bodyCopy = JacksonUtil.createObjectNode();
+        Iterator<Map.Entry<String, JsonNode>> iterator = body.fields();
+        while (iterator.hasNext()) {
+            Map.Entry<String, JsonNode> entry = iterator.next();
+            String replaced = replace(entry.getValue().toString(), parameters);
+            bodyCopy.put(entry.getKey(), replaced);
+        }
+        return RequestBody.create(MediaType.parse("application/json"), bodyCopy.toString());
     }
 
-    protected HttpUriRequest setEntity(HttpEntityEnclosingRequestBase requestBase, StringEntity stringEntity) {
-        requestBase.setEntity(stringEntity);
-        return requestBase;
+    protected String replace(String text, ObjectNode parameters) {
+        PropertyPlaceholder propertyPlaceholder = new PropertyPlaceholder(log, "{", "}");
+        Properties properties = new Properties();
+        Iterator<Map.Entry<String, JsonNode>> iterator = parameters.fields();
+        while (iterator.hasNext()) {
+            Map.Entry<String, JsonNode> entry = iterator.next();
+            properties.put(entry.getKey(), entry.getValue().toString());
+        }
+        return propertyPlaceholder.replacePlaceholders(text, properties);
     }
+
+    protected abstract Request.Builder getRequest(RequestBody body);
 }
